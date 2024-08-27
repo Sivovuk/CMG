a1a31b727ce668e1fae3d3e2bbc2b7a1ccc2f852
Viktor Radovanović <viktorzbpe97@gmail.com>
2024-08-27 11:43:56 +0200

    SFX added
    - I have added sounds for flip, mismatcha and match, also background and end game music
    - Script AudioController manage sounds and music trought out the scenes
    - Script AudioSettings save and load audio settings setup in setting menu in MainMenu scene

---
+++ CMG/Assets/Scripts/SFX/AudioController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    [field:SerializeField] public AudioSource BackgroundMusic { get; private set; }
+    [field:SerializeField] public AudioSource Flip  { get; private set; }
+    [field:SerializeField] public AudioSource GameEnd  { get; private set; }
+    [field:SerializeField] public AudioSource Match  { get; private set; }
+    [field:SerializeField] public AudioSource Mismatch  { get; private set; }
+
+    public static AudioController Instance { get; private set; }
+
+    private void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else 
+            Destroy(gameObject);
+    }
+
+    public void PlayAudio(AudioSource audio) 
+    {
+        if (audio != null) 
+            audio.PlayOneShot(audio.clip);
+    }
+
+    public void StopAudio(AudioSource audio)
+    {
+        if (audio != null)
+            audio.Stop();
+    }
+}
